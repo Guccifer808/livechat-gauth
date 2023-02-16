7537d6545ec24e958a94c895ae0f7337c5345d64
G808 <hucciallstars@gmail.com>
2023-02-16 22:24:43 +0200

    Added condition to visually split replies from other users

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { UserAuth } from "../context/AuthContext";
 
 const Message = ({ message }) => {
+  const { currentUser } = UserAuth();
   return (
     <>
-      <div className="chat chat-start">
+      <div
+        className={`chat ${
+          message.uid === currentUser.uid ? "chat-end" : "chat-start"
+        }`}
+      >
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img src={message.profilePic} />
