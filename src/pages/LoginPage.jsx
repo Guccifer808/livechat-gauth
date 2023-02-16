ba147aec6fd4fb4ea96d9ef772489fd900fe8e8e
G808 <hucciallstars@gmail.com>
2023-02-16 18:38:56 +0200

    Login page edits

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='text-center lg:text-left'>
+          <h1 className='text-5xl font-bold'>Login now!</h1>
+          <p className='py-6'>
+            Currently we support only Google authentication. Please login to
+            join the conversations, find new friends and share ideas!
+          </p>
+        </div>
+        <div className='form-control mt-6'>
+          <button className='btn btn-primary'>Login with Google</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
