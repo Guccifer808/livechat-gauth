b89f6e50557f09d07ba2cecdb8e3745872dc9e36
G808 <hucciallstars@gmail.com>
2023-02-16 20:48:44 +0200

    Installed Firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgJzGRYazL1mhdElH4pN6_QP94bx8gaHs",
+  authDomain: "livechat-gauth.firebaseapp.com",
+  projectId: "livechat-gauth",
+  storageBucket: "livechat-gauth.appspot.com",
+  messagingSenderId: "136195051271",
+  appId: "1:136195051271:web:ed2ec154a13071a61cffa2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
